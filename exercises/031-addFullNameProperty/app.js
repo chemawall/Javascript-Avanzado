e319ffdb0e3ca24ac8f9d4e5f92c6cc9a754c70f
chemawall <jmmuro88@gmail.com>
2023-03-28 16:24:48 +0000

    33

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,4 +1,17 @@
+
+
+let person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+
 function addFullNameProperty(obj) {
   // Add your code after this line
-  
-}
+  obj["fullName"] = obj.firstName +" "+ obj.lastName;
+  return obj
+};
+
+addFullNameProperty(person);
+console.log(person.fullName)
+
+// para añadir a un objeto una nueva key usamos obj["nuevakey"]= valordelakey
