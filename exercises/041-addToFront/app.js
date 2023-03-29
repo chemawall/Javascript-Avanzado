82625cf4f6a696df63ca4513396f7c3f7f2941af
chemawall <jmmuro88@gmail.com>
2023-03-29 15:40:15 +0000

    45

--- exercises/041-addToFront/app.js
+++ exercises/041-addToFront/app.js
@@ -1,8 +1,10 @@
 function addToFront(arr, element) {
   // your code here
+  arr.unshift(element)
+  return arr
 }
 
 let output = addToFront([1, 2], 3);
 console.log(output); // -> [3, 1, 2]
 
-//Hint: you can use unshift
+//importante la diferencia entre usar corchetes (para acceder a la posición del array) y usar paréntesis (para añadir un elemento al array)
