3b9e8ab16c66544fc5941d1b853d16403cae1786
chemawall <jmmuro88@gmail.com>
2023-03-29 15:30:08 +0000

    40

--- exercises/036-isPersonOldEnoughToDrink/app.js
+++ exercises/036-isPersonOldEnoughToDrink/app.js
@@ -1,4 +1,6 @@
-function isPersonOldEnoughToDrink(person) {
+function isPersonOldEnoughToDrink(obj) {
   // Add your code after this line
+    if (obj.age >= 21) return true
+  else return false
   
 }
