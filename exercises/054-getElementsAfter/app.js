091721e9d8f7609c3dd183630036029f15460329
chemawall <jmmuro88@gmail.com>
2023-03-30 14:42:03 +0000

    55

--- exercises/054-getElementsAfter/app.js
+++ exercises/054-getElementsAfter/app.js
@@ -1,5 +1,7 @@
 function getElementsAfter(array, n) {
   // your code here
-  return  array.slice(n,array.length)
+  return  array.slice(n+1,array.length)
   
 }
+let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
+console.log(output)
--- exercises/055-getElementsUpTo/app.js
+++ exercises/055-getElementsUpTo/app.js
@@ -1,4 +1,6 @@
 function getElementsUpTo(array, n) {
   // your code here
-  
+  return  array.slice(0,n-1)
 }
+let output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output);
